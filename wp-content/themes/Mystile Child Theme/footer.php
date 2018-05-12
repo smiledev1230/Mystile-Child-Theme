e3d72c0d62811cec4138a8fac371d8afb9728cda
Web Developer <smilemonkey1230@outlook.com>
2018-05-12 22:24:39 +0700

    make single product page to full width

---
+++ wp-content/themes/Mystile Child Theme/footer.php
@@ -0,0 +1,67 @@
+<?php
+// File Security Check
+if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename( $_SERVER['SCRIPT_FILENAME'] ) ) {
+    die ( 'You do not have sufficient permissions to access this page!' );
+}
+?>
+<?php
+/**
+ * Footer Template
+ *
+ * Here we setup all logic and XHTML that is required for the footer section of all screens.
+ *
+ * @package WooFramework
+ * @subpackage Template
+ */
+	global $woo_options;
+
+	echo '</div><div class="footer-wrap">';// #wrapper
+
+	$total = 4;
+	if ( isset( $woo_options['woo_footer_sidebars'] ) && ( $woo_options['woo_footer_sidebars'] != '' ) ) {
+		$total = $woo_options['woo_footer_sidebars'];
+	}
+
+	if ( ( woo_active_sidebar( 'footer-1' ) ||
+		   woo_active_sidebar( 'footer-2' ) ||
+		   woo_active_sidebar( 'footer-3' ) ||
+		   woo_active_sidebar( 'footer-4' ) ) && $total > 0 ) {
+
+?>
+	<?php woo_footer_before(); ?>
+
+		<section id="footer-widgets" class="col-full col-<?php echo $total; ?> fix">
+
+			<?php $i = 0; while ( $i < $total ) { $i++; ?>
+				<?php if ( woo_active_sidebar( 'footer-' . $i ) ) { ?>
+
+			<div class="block footer-widget-<?php echo $i; ?>">
+	        	<?php woo_sidebar( 'footer-' . $i ); ?>
+			</div>
+
+		        <?php } ?>
+			<?php } // End WHILE Loop ?>
+
+		</section><!-- /#footer-widgets  -->
+	<?php } // End IF Statement ?>
+		<footer id="footer" class="col-full">
+
+			<div id="copyright" class="col-left">
+			<?php if( isset( $woo_options['woo_footer_left'] ) && $woo_options['woo_footer_left'] == 'true' ) {
+
+					echo( stripslashes( apply_filters( 'wpml_translate_single_string', $woo_options['woo_footer_left_text'] , 'admin_texts_woo_footer_left_text', 'woo_footer_left_text' ) ) );
+
+			} else { ?>
+				<p><?php bloginfo(); ?> &copy; <?php echo date( 'Y' ); ?>. <?php _e( 'All Rights Reserved.', 'woothemes' ); ?></p>
+			<?php } ?>
+			</div>
+
+			
+
+		</footer><!-- /#footer  -->
+
+	</div><!-- / footer-wrap -->
+<?php wp_footer(); ?>
+<?php woo_foot(); ?>
+</body>
+</html>
